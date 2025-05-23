bf479cf764333e7e881b8c58567ee0f7cbc737ad
Hitesh485 <74427980+Hitesh485@users.noreply.github.com>
2025-05-23 08:25:27 +0000

    tree trav and isbalance done

---
+++ Trees/isBalance.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+// if height of left subtree & right subtree -> abs(lt - rt) <= 1
+// either -1, 0, 1.
+
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* left = NULL;
+    Node* right = NULL;
+};
+
+Node* buildTree()
+
+int main ()
+{
+    
+    return 0;
+}
