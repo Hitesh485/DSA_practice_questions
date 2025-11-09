3dc1329b8d71f1084dc4ce7c7f0ef66a04760e91
Hitesh485 <74427980+Hitesh485@users.noreply.github.com>
2025-11-09 09:38:14 +0000

    Add new implementations for Reverse Only Letters and Valid Anagram; refactor existing code for clarity

--- Strings/Assignment/reverseOnlyAlphabets.cpp
+++ Strings/Assignment/reverseOnlyAlphabets.cpp
@@ -1,35 +1,47 @@
-class Solution {
-public:
+#include <iostream>
 
-    bool checkAlpha(char ch)
+using namespace std;
+
+bool isChar(char ch)
+{
+    if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
     {
-        if ((ch >= 97 && ch <= 122) || (ch >= 65 && ch <= 90))
-        {
-            return true;
-        }
-        return false;
+        return true;
     }
+    return false;
+}
 
-    string reverseOnlyLetters(string s) {
-        int i = 0;
-        int j = s.size()-1;
-        while (i < j)
+string reverseOnlyLetters(string str)
+{
+    // swap
+    int i = 0;
+    int j = str.length()-1;
+
+    while (i < j)
+    {
+        if (isChar(str[i]) && isChar(str[j]))
         {
-            if (checkAlpha(s[i]) && checkAlpha(s[j]))
-            {
-                swap(s[i], s[j]);
-                i++;
-                j--;
-            }
-            else if (!checkAlpha(s[i]))
-            {
-                i++;
-            }
-            else
-            {
-                j--;
-            }
+            swap(str[i], str[j]);
+            i++;
+            j--;
         }
-        return s;
+        else if (!isChar(str[i]))
+        {
+            i++;
+        }
+        else
+        {
+            j--;
+        }
     }
-};
+    return str;
+}
+
+int main ()
+{
+    
+    // string str = "a-bC-dEf-ghIj";
+    string str = "Test1ng-Leet=code-Q!";
+    cout << reverseOnlyLetters(str) << endl;
+    return 0;
+}
--- Strings/Assignment/valid_anagram.cpp
+++ Strings/Assignment/valid_anagram.cpp
@@ -1,69 +1,41 @@
-class Solution
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool validAnagram(string s, string t)
 {
-public:
-    bool isAnagram(string s, string t)
+    vector<int> freqTable(256, 0);
+
+    for (int i = 0; i < s.size(); i++)
     {
-
-        // edge case ->
-        if (s.size() != t.size())
-        {
-            return false;
-        }
-
-        // we have to achieve the same count of each char.
-
-        unordered_map<int, int> mp1;
-        for (int i = 0; i < s.size(); i++)
-        {
-            mp1[s[i]]++;
-        }
-
-        unordered_map<int, int> mp2;
-        for (int i = 0; i < t.size(); i++)
-        {
-            mp2[t[i]]++;
-        }
-
-        // check occrr is same in s string or not
-        for (int i = 0; i < t.size(); i++)
-        {
-            char key = t[i];
-            if (mp1[key] != mp2[key])
-            {
-                return false;
-            }
-        }
-        return true;
+        int ch = s[i];
+        freqTable[ch]++;
     }
-}; 
 
-// method -2 optimize
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        // more optimize method -> 
-        // going to maintain a freq table 0-255 (ascii).
-
-        int freqTable[256] = {0};
-        for (int i = 0; i < s.size(); i++)
-        {
-            freqTable[s[i]]++;
-        }
-
-        // now, we have dec the count for t string,
-        for (int i= 0;i < t.size(); i++)
-        {
-            freqTable[t[i]]--;
-        }
-
-        // All values of freqTable should be 0, if it is anagram
-        for (int i= 0;i < 256; i++)
-        {
-            if (freqTable[i] != 0)
-            {
-                return false;
-            }
-        }
-        return true;
+    for (int i = 0; i < t.size(); i++)
+    {
+        int ch = t[i];
+        freqTable[ch]--;
     }
-};
+
+    for (auto iter : freqTable)
+    {
+        if (iter != 0) return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    // string s = "anagram";
+    // string t = "nagaram";
+    string s = "cat";
+    string t = "rat";
+
+    cout << validAnagram(s, t) << endl;
+
+    return 0;
+}
